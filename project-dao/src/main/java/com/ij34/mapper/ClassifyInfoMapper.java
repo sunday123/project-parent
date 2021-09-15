166f7e6de8fc2842b0b35714fc3e15e16540c7f3
sunday123 <695200158@qq.com>
2021-09-15 18:32:37 +0800

    tk--json

--- project-dao/src/main/java/com/ij34/mapper/ClassifyInfoMapper.java
+++ project-dao/src/main/java/com/ij34/mapper/ClassifyInfoMapper.java
@@ -7,14 +7,7 @@ import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
-/**
- * <p>
- * 废品回收:大类、小类、规格 Mapper 接口
- * </p>
- *
- * @author gdlv
- * @since 2021-05-23
- */
+
 @Repository
 public interface ClassifyInfoMapper extends Mapper<ClassifyInfo> {
 
